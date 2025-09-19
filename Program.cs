4776799b44a099826a14ee59e90abe3fd5b63ffe
luccve <hurryblank@gmail.com>
2025-09-19 18:57:14 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokiaTeste = new Nokia("11113123123", "1100", "21021231", 512);
+
+Smartphone iphoneTeste = new Iphone("11113123123", "31", "21021231", 312);
+
+Console.WriteLine("Operando o Nokia");
+nokiaTeste.Ligar();
+nokiaTeste.ReceberLigacao();
+nokiaTeste.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nOperando o Iphone");
+iphoneTeste.Ligar();
+iphoneTeste.InstalarAplicativo("Whatsapp");
+iphoneTeste.ReceberLigacao();
